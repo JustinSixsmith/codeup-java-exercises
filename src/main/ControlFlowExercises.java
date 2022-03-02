3510ebebb3b98e75813be048cdd2440559b13d8e
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-02 10:21:40 -0600

    adds exercise 1b3

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -10,11 +10,17 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
-        int i = 100;
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+
+        long i = 2;
         do {
             System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+            i *= i;
+        } while (i < 1000000);
 
 
     }
