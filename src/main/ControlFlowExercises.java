08cbf3f9b8997bada8573ab0746e7ccef66fac93
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-02 11:13:12 -0600

    adds exercise 2, fizzbuzz

--- src/main/ControlFlowExercises.java
+++ src/main/ControlFlowExercises.java
@@ -16,12 +16,24 @@ public class ControlFlowExercises {
 //            }
 //        }
 
-        for (long k = 2; k < 1000000; k++) {
-            System.out.println(k);
-            k *= k;
-            k--;
-    }
+//        for (long k = 2; k < 1000000; k++) {
+//            System.out.println(k);
+//            k *= k;
+//            k--;
+//    }
 
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
 
 }
 }
