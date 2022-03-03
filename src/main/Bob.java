47388a6bfa07df377a22074dee6cb72b4e7d3637
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-03 10:09:40 -0600

    refactors bob

--- src/main/Bob.java
+++ src/main/Bob.java
@@ -10,15 +10,28 @@ public class Bob {
         System.out.println("Yeah? ");
         String saidToBob = scanner.nextLine();
 
-        if (saidToBob.endsWith("?")) {
-            System.out.println("Sure.");
-        } else if (saidToBob.endsWith("!")) {
-            System.out.println("Whoa, chill out!");
-        }else if (saidToBob.equals("")) {
-            System.out.println("Fine. Be that way!");
-        } else {
-            System.out.println("Whatever.");
-        }
+        boolean more = true;
+
+        do {
+            if (saidToBob.endsWith("?")) {
+                System.out.println("Sure.");
+            } else if (saidToBob.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (saidToBob.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever.");
+            }
+
+            System.out.println("Is that it (y)? ");
+            saidToBob = scanner.nextLine();
+
+            if (saidToBob.equalsIgnoreCase("y")) {
+                more = false;
+            }
+        } while (more);
+
+        System.out.println("kbye.");
 
         scanner.close();
     }
