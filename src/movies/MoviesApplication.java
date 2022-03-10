eba3e6e15fe830737e18d10ea127179b883996e2
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-10 17:02:38 -0600

    attempts bonus

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -9,7 +9,7 @@ public class MoviesApplication {
     public static void main(String[] args) {
         Input input = new Input();
 
-        Movie[] movies = MoviesArray.findAll();
+        Movie[] allMovies = MoviesArray.findAll();
 
         int choice;
 
@@ -21,51 +21,61 @@ public class MoviesApplication {
                     "2 - view movies in the animated category\n" +
                     "3 - view movies in the drama category\n" +
                     "4 - view movies in the horror category\n" +
-                    "5 - view movies in the scifi category\n\n");
-            choice = input.getInt("Enter your choice: ", 0, 5);
+                    "5 - view movies in the scifi category\n" +
+                    "6 - add a new movie\n\n");
+            choice = input.getInt("Enter your choice: ", 0, 6);
 
             if (choice == 1) {
-                for (int i = 0; i < movies.length; i++) {
-                    System.out.println(movies[i].getName() + " -- " + movies[i].getCategory());
+
+                for (int i = 0; i < allMovies.length; i++) {
+                    System.out.println(allMovies[i].getName() + " -- " + allMovies[i].getCategory());
                 }
 
             }
             if (choice == 2) {
 
-                for (int i = 0; i < movies.length; i++) {
-                    String category = movies[i].getCategory();
+                for (int i = 0; i < allMovies.length; i++) {
+                    String category = allMovies[i].getCategory();
                     if (category == "animated")
-                        System.out.println(movies[i].getName() + " -- " + movies[i].getCategory());
+                        System.out.println(allMovies[i].getName() + " -- " + allMovies[i].getCategory());
                 }
             }
             if (choice == 3) {
 
-                for (int i = 0; i < movies.length; i++) {
-                    String category = movies[i].getCategory();
+                for (int i = 0; i < allMovies.length; i++) {
+                    String category = allMovies[i].getCategory();
                     if (category == "drama")
-                        System.out.println(movies[i].getName() + " -- " + movies[i].getCategory());
+                        System.out.println(allMovies[i].getName() + " -- " + allMovies[i].getCategory());
                 }
             }
             if (choice == 4) {
 
-                for (int i = 0; i < movies.length; i++) {
-                    String category = movies[i].getCategory();
+                for (int i = 0; i < allMovies.length; i++) {
+                    String category = allMovies[i].getCategory();
                     if (category == "horror")
-                        System.out.println(movies[i].getName() + " -- " + movies[i].getCategory());
+                        System.out.println(allMovies[i].getName() + " -- " + allMovies[i].getCategory());
                 }
             }
             if (choice == 5) {
 
-                for (int i = 0; i < movies.length; i++) {
-                    String category = movies[i].getCategory();
+                for (int i = 0; i < allMovies.length; i++) {
+                    String category = allMovies[i].getCategory();
                     if (category == "scifi")
-                        System.out.println(movies[i].getName() + " -- " + movies[i].getCategory());
+                        System.out.println(allMovies[i].getName() + " -- " + allMovies[i].getCategory());
                 }
             }
+//            if (choice == 6) {
+//                String newName = input.getString("Please enter the movie name: ");
+//                String newCategory = input.getString("Please enter the movie category: ");
+//                allMovies[allMovies.length] = new Movie(newName, newCategory);
+//            }
             System.out.println();
         } while (choice > 0);
 
 
         input.close();
+    }
+    public static void addMovie(String name, String category) {
+        Movie newMovie = new Movie(name, category);
     }
 }
