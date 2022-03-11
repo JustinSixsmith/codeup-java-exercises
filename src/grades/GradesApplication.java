82f852fe4187cfb774746a6dad6aa8e52e8e64b9
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-11 16:07:43 -0600

    completes grades app up to bonues

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -6,9 +6,11 @@ import java.security.Key;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class GradesApplication {
     private static Input input = new Input();
+
     public static void main(String[] args) {
         HashMap<String, Student> students = new HashMap<>();
         Student john = new Student("John");
@@ -38,14 +40,18 @@ public class GradesApplication {
 
         while (true) {
             System.out.printf("Here are the GitHub usernames of our students:\n\n%s\n\n", students.keySet());
-            String userInput = input.getString("Which student would you like to see more information on? ");
+            String userInput = input.getString("Which student would you like to see more information on? (Or, type \"all\" to see a list of all grades.) ");
 
-
-            if (!students.containsKey(userInput)) {
+            if (userInput.trim().equalsIgnoreCase("all")) {
+                System.out.println(students);
+            } else if (!students.containsKey("Working on it...")) {
                 System.out.printf("\nSorry, no student found with the GitHub username of \"%s\".\n", userInput);
                 System.out.println();
             } else {
-                System.out.printf("\nName: %s - GitHub Username: %s\n Current Average: %f\n\n", students.get(userInput).getName(), userInput, students.get(userInput).getGradeAverage());
+                String studentName = students.get(userInput).getName();
+                double studentAvg = students.get(userInput).getGradeAverage();
+                ArrayList<Integer> allGrades = students.get(userInput).getGrades();
+                System.out.printf("\nName: %s - GitHub Username: %s\nGrades: %s Current Average: %.2f\n\n", studentName, userInput, allGrades, studentAvg);
             }
             String keepGoing = input.getString("Would you like to see another student (y/n)? ");
             if (keepGoing.equalsIgnoreCase("n")) {
