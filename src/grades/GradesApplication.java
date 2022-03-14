1fee692a16d1a56f4a36298fa611c19ee2fc783a
Justin Sixsmith <justinsixsmith@gmail.com>
2022-03-13 22:32:52 -0500

    adds printout of all students

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -3,10 +3,7 @@ package grades;
 import util.Input;
 
 import java.security.Key;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Objects;
+import java.util.*;
 
 public class GradesApplication {
     private static Input input = new Input();
@@ -43,16 +40,26 @@ public class GradesApplication {
             String userInput = input.getString("Which student would you like to see more information on? (Or, type \"all\" to see a list of all grades.) ");
 
             if (userInput.trim().equalsIgnoreCase("all")) {
-                System.out.println("Working on it...\n");
+
+                Set eachStudent = students.keySet();
+                for (Object student : eachStudent) {
+                    String studentName = students.get(student).getName();
+                    double studentAvg = students.get(student).getGradeAverage();
+                    ArrayList<Integer> allGrades = students.get(student).getGrades();
+                    System.out.printf("\nName: %s - GitHub Username: %s\nGrades: %s Current Average: %.2f\n\n", studentName, student, allGrades, studentAvg);
+                }
+
             } else if (!students.containsKey(userInput)) {
                 System.out.printf("\nSorry, no student found with the GitHub username of \"%s\".\n", userInput);
                 System.out.println();
+
             } else {
                 String studentName = students.get(userInput).getName();
                 double studentAvg = students.get(userInput).getGradeAverage();
                 ArrayList<Integer> allGrades = students.get(userInput).getGrades();
                 System.out.printf("\nName: %s - GitHub Username: %s\nGrades: %s Current Average: %.2f\n\n", studentName, userInput, allGrades, studentAvg);
             }
+
             String keepGoing = input.getString("Would you like to see another student (y/n)? ");
             if (keepGoing.equalsIgnoreCase("n")) {
                 System.out.println("\nGoodbye, and have a wonderful day!");
